49b97a71d640b4af4dc6a071fd258375a30b298c
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2019-01-25 12:57:13 +0000

    Minor changes

--- Checkpoint 1/ising_model.py
+++ Checkpoint 1/ising_model.py
@@ -76,7 +76,7 @@ class DynamicSystem:
 
 
 class Lattice:
-    def __init__(self, N, sys, points_needed, ds, sps=5000):
+    def __init__(self, N, sys, points_needed, ds, sps=2500):
         self.N = N
         self.sys = sys
         self.pneeded = points_needed  # Number of random points needed
@@ -218,18 +218,18 @@ def main():
     # sys = int(input("Choose the system dynamics: 0, %s: 1, %s: " % (dynamic_sys[0], dynamic_sys[1])))
     # print(dynamic_sys[sys][0])
 
-    lattice = Lattice(50, ds.glauber_dynamics, 1, ds=ds)
-    # lattice = Lattice(50, ds.kawasaki_dynamics, 2, ds=ds)
+    # lattice = Lattice(50, ds.glauber_dynamics, 1, ds=ds)
+    lattice = Lattice(50, ds.kawasaki_dynamics, 2, ds=ds)
 
-    # lattice.run_sim(100)  # Run for a certain number of sweeps.
+    lattice.run_sim(100)  # Run for a certain number of sweeps.
     # lattice.imshow_grid()  # Display grid after n sweeps.
     # lattice.sys_magnetisation()
     # plt.show()
     #
     # lattice.animate()  # Animate live
     #
-    lattice.temperature_tests()  # Run Tests
-    lattice.susceptibility()
+    # lattice.temperature_tests()  # Run Tests
+    # lattice.susceptibility()
     #
     # lattice.sys_magnetisation()
 main()
