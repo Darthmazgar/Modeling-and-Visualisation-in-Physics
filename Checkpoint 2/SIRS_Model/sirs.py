d99a3c3a952d80267f492e553f7d825852dd8639
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2019-02-15 15:12:18 +0000

    sirs model basic working.

--- Checkpoint 2/SIRS_Model/sirs.py
+++ Checkpoint 2/SIRS_Model/sirs.py
@@ -11,16 +11,45 @@ class Sirs:
         self.p2 = p2
         self.p3 = p3
         # 0: suceptable, 1: infected, 2: recovered.
-        self.grid = np.random.choice([0, 1, 2], size=(N, M), p=[1/3, 1/3, 1/3])
+        self.grid = np.random.choice([0, 1], size=(N, M), p=[1/2, 1/2])
+        self.sweeps = 1
         if anim:
             self.fig = plt.figure()
 
-    def update(self, k, anim=True):
+    def check_nn(self, i, j):
+        if self.grid[(i + 1 +self.N) % self.N][j] == 1:
+            return 1
+        elif self.grid[(i - 1 +self.N) % self.N][j] == 1:
+            return 1
+        elif self.grid[i][(j + 1 + self.M) % self.M] == 1:
+            return 1
+        elif self.grid[i][(j - 1 + self.M) % self.M] == 1:
+            return 1
+        else:
+            return 0
 
+    def update(self, k, anim=True):
+        for k in range(self.sweeps):
+            rand_ar = np.random.random((self.N,self.M))
+            for i in range(self.N):
+                for j in range(self.M):
+                    state = self.grid[i][j]
+                    if state == 0:
+                        p = self.p1
+                    elif state == 1:
+                        p = self.p2
+                    elif state == 2:
+                        p = self.p3
+                    if state == 0:
+                        if self.check_nn(i, j) and rand_ar[i][j] <= p:
+                            self.grid[i][j] = (self.grid[i][j] + 1) % 3
+                    else:
+                        if rand_ar[i][j] <= p:
+                            self.grid[i][j] = (self.grid[i][j] + 1) % 3
         if anim:
             self.fig.clear()
-            plt.imshow(self.grid, interpolation='None',
-                       cmap='Blues', vmin=0, vmax=1)
+            plt.imshow(self.grid, interpolation='nearest',
+                       cmap='Blues', vmin=0, vmax=2)
         return self.grid
 
     def run_animation(self):
@@ -51,6 +80,7 @@ def main(argv):
     P2 = float(argv[3])
     P3 = float(argv[4])
 
-    s = Sirs(5, 5, 0.1, 0.1, 0.1)
+    s = Sirs(M, N, P1, P2, P3)
+    s.run_animation()
 
 main(sys.argv[1:])
