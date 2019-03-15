a403ff4cb4011de562ca547d60827789ce96b510
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2019-03-15 13:26:20 +0000

    Start CahnHilliard for CP3.

---
+++ Checkpoint 3/cahn_hilliard.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+class CanHil:
+    def __init__(self, N, M):
+        self.N = N
+        self.M = M
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
